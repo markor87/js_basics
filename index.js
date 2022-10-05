c2353313e6355f400f3c6c1155f43ac617e12627
Badji87 <mradovanovic@msn.com>
2022-10-05 19:56:22 +0200

    commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function greet(){
+/*let username = window.prompt("Koje je tvoje ime?")
+console.log(username);*/
 
+let username;
+document.getElementById("myButton").onclick = function () {
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = username;
 }
